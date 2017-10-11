76ddd303858cf51e562f17d93a7fbd0054716ed5
Philipp Gorczak <p.gorczak@gmail.com>
2017-10-11 17:08:55 +0200

    Finish inference.

--- Gaussian Process/utilities.py
+++ Gaussian Process/utilities.py
@@ -19,13 +19,97 @@ def plot_samples(t, Zs, wrap, size=(1.5, 1.5)):
         t=np.tile(t, n_samples),
         Z=Zs.ravel()))
     w, h = size
+    sns.set_style('ticks')
     grid = sns.FacetGrid(df, col='sample', hue='sample', col_wrap=wrap,
                          size=h, aspect=w/h)
     grid.map(plt.plot, 't', 'Z', marker="o", ms=4)
     plt.show()
 
 
-def plot_gp(mu, k, t, dimension=(2, 4), size=(1.5, 1.5)):
+def mean(mu, t):
+    """Evaluate mean function for index-set t.
+
+    Args:
+        mu: mean function
+        t: index set
+
+    Returns: mean output for t.
+    """
+    return np.array([mu(i) for i in t])
+
+
+def covariance(k, a, b):
+    """Evaluate covariance function for index-sets a and b.
+
+    Args:
+        k: covariance function
+        a, b: two index-sets (arrays)
+
+    Returns: a x b covariance matrix.
+    """
+    return np.array([[k(i, j) for j in b] for i in a])
+
+
+def posterior(mu, k, ta, tb, Zb):
+    """Posterior GP given some observations.
+
+    Args:
+        mu: mean function.
+        k: covariance function.
+        ta: posterior process indices.
+        tb: observation indices.
+        Zb: observation outputs.
+
+    Returns:
+        m: mean of posterior over ta.
+        D: covariance of posterior over ta x ta.
+    """
+    mua = mean(mu, ta)
+    Kaa = covariance(k, ta, ta)
+
+    mub = mean(mu, tb)
+    Kbb = covariance(k, tb, tb)
+
+    Kab = covariance(k, ta, tb)
+
+    Kbb_inv = np.linalg.inv(Kbb)
+    m = mua + Kab.dot(Kbb_inv).dot(Zb - mub)
+    D = Kaa - Kab.dot(Kbb_inv).dot(Kab.T)
+
+    return m, D
+
+
+def noisy_posterior(mu, k, sigma, ta, tb, Zb):
+    """Noisy posterior GP given some observations.
+
+    Args:
+        mu: mean function.
+        k: covariance function.
+        sigma: output observation variance.
+        ta: posterior process indices.
+        tb: observation indices.
+        Zb: observation outputs.
+
+    Returns:
+        m: mean of posterior over ta.
+        D: covariance of posterior over ta x ta.
+    """
+    mua = mean(mu, ta)
+    Kaa = covariance(k, ta, ta)
+
+    mub = mean(mu, tb)
+    Kbb = covariance(k, tb, tb)
+
+    Kab = covariance(k, ta, tb)
+
+    Kbb_inv = np.linalg.inv(Kbb + sigma**2 * np.eye(len(tb)))
+    m = mua + Kab.dot(Kbb_inv).dot(Zb - mub)
+    D = (Kaa + sigma**2 * np.eye(len(ta))) - Kab.dot(Kbb_inv).dot(Kab.T)
+
+    return m, D
+
+
+def plot_gp(mu, k, t, dimension=(2, 6), size=(1.5, 1.5)):
     """Plot samples of a Gaussian process.
 
     Args:
@@ -37,8 +121,8 @@ def plot_gp(mu, k, t, dimension=(2, 4), size=(1.5, 1.5)):
     Returns
         Mean and covariance matrix of the GP-distribution.
     """
-    mu_ = np.array([mu(i) for i in t])
-    C = np.array([[k(i, j) for j in t] for i in t])
+    mu_ = mean(mu, t)
+    C = covariance(k, t, t)
     h, w = dimension
     samples = np.random.multivariate_normal(mu_, C, h * w)
     plot_samples(t, samples, w, size)
